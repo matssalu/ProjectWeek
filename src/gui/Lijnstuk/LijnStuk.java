3d292faff48c6e37aa2e9ff63611c6dd10496c96
matssalu <mats.salu@gmail.com>
2021-03-30 10:21:26 +0200

    Lijnstuk

---
+++ src/gui/Lijnstuk/LijnStuk.java
@@ -0,0 +1,45 @@
+package gui.Lijnstuk;
+import gui.Punt.Punt;
+
+public class LijnStuk {
+
+    private Punt startPunt;
+    private Punt eindPunt;
+
+    public LijnStuk(Punt startPunt, Punt eindPunt) {
+        setStartEinde(startPunt, eindPunt);
+        setStartPunt(startPunt);
+        setEindpunt(eindPunt);
+    }
+
+    private void setStartPunt(Punt startPunt) {
+        if(startPunt == null) throw new ("startpunt mag niet null zijn!");
+        this.startPunt = startPunt;
+    }
+
+    private void setEindpunt(Punt eindPunt) {
+        if(eindPunt == null) throw new IllegalArgumentException("eindpunt mag niet null zijn!");
+        this.eindPunt = eindPunt;
+    }
+
+    private void setStartEinde(Punt startPunt, Punt eindPunt) {
+        if(startPunt == null || eindPunt == null || startPunt.toString() == eindPunt.toString()) throw new IllegalArgumentException("Punten mogen niet gelijk aan elkaar zijn!");
+    }
+
+    public Punt getStartPunt() {
+        return this.startPunt;
+    }
+
+    public Punt getEindPunt() {
+        return this.eindPunt;
+    }
+
+    public boolean equals(LijnStuk andere){
+        if(this == andere) return true;
+        return false;
+    }
+
+    public String toString(){
+        return "Lijn: (" + startPunt.getX() + ", " + startPunt.getY() + ") - eindpunt: (" + eindPunt.getX() + ", " + eindPunt.getY() + ")";
+    }
+}
---
+++ src/gui/Lijnstuk/LijnStukTest.java
@@ -0,0 +1,50 @@
+package gui.Lijnstuk;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import gui.Lijnstuk.LijnStuk;
+import gui.Punt.Punt;
+import org.junit.Test;
+
+public class LijnStukTest {
+    private Punt punt1 = new Punt(10, 20);
+    private Punt zelfdeAlsPunt1 = new Punt(10, 20);
+    private Punt punt2 = new Punt(190, 30);
+    private Punt zelfdeAlsPunt2 = new Punt(190, 30);
+
+    @Test
+    public void LijnStuk_moet_lijnstuk_aanmaken_met_gegeven_startPunt_en_eindPunt() {
+        LijnStuk lijnstuk = new LijnStuk(punt1, punt2);
+
+        assertEquals(punt1, lijnstuk.getStartPunt());
+        assertEquals(punt2, lijnstuk.getEindPunt());
+    }
+
+    @Test (expected = DomainException.class)
+    public void LijnStuk_Moet_exception_gooien_als_startpunt_null()  {
+        new LijnStuk(null, punt2);
+    }
+
+    @Test (expected = DomainException.class)
+    public void LijnStuk_Moet_exception_gooien_als_eindpunt_null()  {
+        new LijnStuk(punt1, null);
+    }
+
+    @Test
+    public void equals_moet_true_teruggeven_als_begin_en_eindpunt_gelijk_zijn(){
+        LijnStuk lijnStuk = new LijnStuk(punt1, punt2);
+        LijnStuk zelfdeLijnStuk = new LijnStuk(zelfdeAlsPunt1, zelfdeAlsPunt2);
+        assertTrue(lijnStuk.equals(zelfdeLijnStuk));
+    }
+
+    @Test
+    public void equals_moet_false_teruggeven_als_parameter_null(){
+        LijnStuk lijnStuk = new LijnStuk(punt1, punt2);
+        assertFalse(lijnStuk.equals(null));
+    }
+
+    private class DomainException extends Throwable {
+    }
+}
