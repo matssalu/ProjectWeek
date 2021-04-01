24dfe3825fa8aa4d2ccd0d6163f50f2753feede5
matssalu <mats.salu@gmail.com>
2021-04-01 14:24:49 +0200

    quick fix

--- src/Test/AllTests.java
+++ src/Test/AllTests.java
@@ -2,6 +2,7 @@ package Test;
 
 import domain.Cirkel.CirkelTest;
 import domain.Driehoek.DriehoekTest;
+import domain.Hangman.HangManTest;
 import domain.HintLetterTest;
 import domain.HintWoord.HintWoordTest;
 import domain.Lijnstuk.LijnStukTest;
@@ -17,6 +18,6 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ PuntTest.class, SpelerTest.class, CirkelTest.class, LijnStukTest.class, RechthoekTest.class, DriehoekTest.class, HintLetterTest.class, HintWoordTest.class, OmhullendeTest.class, TekeningTest.class, WoordenLijstTest.class})
+@SuiteClasses({ PuntTest.class, SpelerTest.class, CirkelTest.class, LijnStukTest.class, RechthoekTest.class, DriehoekTest.class, HintLetterTest.class, HintWoordTest.class, OmhullendeTest.class, TekeningTest.class, WoordenLijstTest.class, HangManTest.class})
 public class AllTests {
 }
